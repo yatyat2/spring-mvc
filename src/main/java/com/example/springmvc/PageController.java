7bcc19fb833ba975494b970c0ad98f1b08982840
yatyat2 <xowls2287@naver.com>
2022-01-20 21:27:39 +0900

    form, list 페이지 추가

--- src/main/java/com/example/springmvc/PageController.java
+++ src/main/java/com/example/springmvc/PageController.java
@@ -1,27 +1,35 @@
 package com.example.springmvc;
 
+import com.example.springmvc.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PageController {
+    UserRepository userRepository = new UserRepository();
 
     @GetMapping("/form")
-    public String form(){
+    public String form(Model model){
+        model.addAttribute("user",new User("",""));
         return "form";
     }
 
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
+        model.addAttribute("users", userRepository.users);
         return "list";
     }
 
-//    @GetMapping("/details")
-//    public String form(){
-//        return "form";
-//    }
+    @PostMapping("/list")
+    public String createUser(@ModelAttribute User user, Model model){
+        userRepository.createUser(user);
+        model.addAttribute("users", userRepository.users);
+        return "list";
 
-
+    }
 }
 
 
